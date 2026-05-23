11769d0278b1dafed7857982435e707b5a27084e
dianaerazo <00149823@uca.edu.sv>
2026-05-23 11:59:44 -0600

    entity Specimen and dto

---
+++ Laboratorio-3/src/main/java/org/example/laboratorio3/domain/dto/response/specimen/SpecimenResponse.java
@@ -0,0 +1,16 @@
+package org.example.laboratorio3.domain.dto.response.specimen;
+
+import lombok.*;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SpecimenResponse {
+    private UUID id;
+    private String name;
+    private String region;
+    private Integer dangerLevel;
+    private Boolean isFriendly;
+}
